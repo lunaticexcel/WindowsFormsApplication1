2de064233f8c10eb371d5264c86bbefab6c0b6c7
Hiromichi Kawashima <kichi@cosmicblue.org>
2012-11-30 00:05:37 +0900

    とりあえず

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(173, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        //時間経過をはかるためのクラス
+        Stopwatch myStopWatch = new Stopwatch();
+        //スタート・ストップボタン用
+        Boolean sw = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //各コントロールのプロパティを設定する
+
+            //フォームのプロパティ
+            this.Size = new Size(300, 180);
+            this.Text = "ストップウォッチ";
+
+            //ラベルのプロパティ
+            this.label1.Text = myStopWatch.Elapsed.ToString();
+            this.label1.BackColor = Color.White;
+            this.label1.Font = new System.Drawing.Font("富士ポップ", 36);
+            this.label1.BorderStyle = BorderStyle.Fixed3D;
+            this.label1.Location = new Point(0, 9);
+            this.label1.Size = new Size(290, 50);
+            this.label1.TextAlign = ContentAlignment.TopCenter;
+            this.label1.AutoSize = false;
+
+            //スタート・ストップボタンのプロパティ
+            this.button1.Text = "スタート";
+            this.button1.Size = new Size(85, 30);
+            this.button1.Location = new Point(40, 100);
+
+            //リセットボタンのプロパティ
+            this.button2.Text = "リセット";
+            this.button2.Size = new Size(85, 30);
+            this.button2.Location = new Point(160, 100);
+            this.button2.Enabled = false;
+
+            //表示更新用タイマーのプロパティ
+            this.timer1.Interval = 10;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //スイッチがoff（計測開始していない）とき
+            if (sw == false)
+            {
+                //計測開始
+                myStopWatch.Start();
+                //表示更新タイマー開始
+                timer1.Start();
+                //スイッチon
+                sw = true;
+                //リセットボタン使用不可
+                button2.Enabled = false;
+                //「スタート」だったボタンの表示を「ストップ」に変更
+                button1.Text = "ストップ";
+            }
+
+            //スイッチがoff以外のとき（つまりはonのとき）
+            else
+            {
+                //計測終了
+                myStopWatch.Stop();
+                //表示固定
+                timer1.Stop();
+                //スイッチoff
+                sw = false;
+                //リセットボタン使用可
+                button2.Enabled = true;
+                //「ストップ」だったボタンの表示を「スタート」に変更
+                button1.Text = "スタート";
+            }  
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //ストップウォッチの内容をゼロにする
+            myStopWatch.Reset();
+            //リセットした状態をlabel1に表示する
+            label1.Text = myStopWatch.Elapsed.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //label1にスタートから現在までの時間を表示させる
+            label1.Text = myStopWatch.Elapsed.ToString();
+        }
+    }
+}
